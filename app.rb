a42671429b4e59190d0d27825c8a60c40c3c98a7
Lori Henderson <lorim95@gmail.com>
2022-06-23 22:59:31 -0400

    commit tree a60cd057a2c43f6acf9f9643cdeda036515fe2ca

---
+++ app.rb
@@ -0,0 +1,12 @@
+# app.rb
+# puts === console.log()
+# run code with ruby app.rb
+#IRB - Interactive Ruby
+# puts - adds a line break at the end of the string
+puts "Hello World!"
+# print - does not add a line break at the end of the string
+print "Pass this test, please."
+# p - outputs the data in a nicer format by calling the .inspect method on our data (best for arrays)
+p [1, 2, 3]
+# pp - "pretty printing" complex data, such as nested arrays and hashes
+# pp [{id: 1, hello: "world"}, {id: 2, hello: "Ruby"}, {id: 3, hello: "Moon"}]
